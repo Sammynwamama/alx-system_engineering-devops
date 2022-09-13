64857e3a92e95f22b02fb48925366c20b582ee61
Sammynwamama <Sammynwamama@gmail.com>
2022-09-13 12:44:23 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef HOLOBERTON_H_INCLUDED
+#Define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha9int(int c);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
